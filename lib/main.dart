d325a9b63492a499beb7275d6cf099c75c1c2d5b
sanayut123 <sanayut4112_@hotmail.com>
2020-08-17 10:40:20 -0700

    card widget

--- lib/main.dart
+++ lib/main.dart
@@ -39,43 +39,43 @@ class MyApp extends StatelessWidget {
 class layoutWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return GridView.count(
-      crossAxisCount: 2,
-      children: [
-        Container(
-          height: 100.0,
-          color: Colors.red,
-        ),
-        Container(
-          height: 100.0,
-          color: Colors.blue,
-        ),
-        Container(
-          height: 100.0,
-          color: Colors.green,
-        ),
-        Container(
-          height: 100.0,
-          color: Colors.orange,
-        ),
-        Container(
-          height: 100.0,
-          color: Colors.purple,
-        ),
-        Container(
-          height: 100.0,
-          color: Colors.yellow,
-        ),
-        Container(
-          height: 100.0,
-          color: Colors.grey,
-        ),
-        Container(
-          height: 100.0,
-          color: Colors.pink,
-        ),        
-      ],
-    );
+    return Card(
+      clipBehavior: Clip.hardEdge,
+      margin: EdgeInsets.all(10.0),
+      color: Colors.red[50],
+      shadowColor: Colors.black,
+      child: Column(
+        children: [
+          ListTile(
+            title: Text('1625 Main Street',
+                style: TextStyle(fontWeight: FontWeight.w500)),
+            subtitle: Text('My City, CA 99984'),
+            leading: Icon(
+              Icons.restaurant_menu,
+              color: Colors.blue[500],
+            ),
+
+          ),
+          Divider(),
+          ListTile(
+            title: Text('(408) 555-1212',
+                style: TextStyle(fontWeight: FontWeight.w500)),
+            leading: Icon(
+              Icons.contact_phone,
+              color: Colors.blue[500],
+            ),
+          ),
+          Divider(),
+          ListTile(
+            title: Text('costa@example.com'),
+            leading: Icon(
+              Icons.contact_mail,
+              color: Colors.blue[500],
+            ),
+          ),
+        ],
+      )
+      );
   }
 }
 
